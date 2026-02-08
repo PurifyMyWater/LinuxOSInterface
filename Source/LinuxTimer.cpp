654b09150e9c08d6af48f9d4ff2d4e838e9d442d
Alexander Zerpa <alexander.zerpa@purifymywater.org>
2026-02-08 22:59:30 +0100

    feat: add `LinuxTimer` implementation

---
+++ Source/LinuxTimer.cpp
@@ -0,0 +1,123 @@
+#include "LinuxTimer.h"
+#include "OSInterface_Log.h"
+
+#include <cstdlib>
+#include <cstring>
+
+void LinuxTimer::callbackWrapper(union sigval sv)
+{
+    if (LinuxTimer* timer = static_cast<LinuxTimer*>(sv.sival_ptr);
+        timer != nullptr && timer->callbacFunction != nullptr)
+    {
+        OSInterfaceLogInfo("LinuxOSInterface", "timer callback invoked");
+        timer->callbacFunction(timer->callbackArg);
+    }
+    else
+    {
+        OSInterfaceLogWarning("LinuxOSInterface", "Timer callback invoked but %s%s",
+                              timer == nullptr ? "EspTimer instance is null" : "callback function is null for timer ",
+                              timer == nullptr ? "" : timer->name);
+    }
+}
+
+LinuxTimer::LinuxTimer(uint32_t period, OSInterface_Timer::Mode mode, OSInterfaceProcess callback, void* callbackArg,
+                       const char* timerName)
+{
+    this->name = strdup(timerName);
+
+    this->callbacFunction = callback;
+    this->callbackArg    = callbackArg;
+
+    this->timerSpec.it_value.tv_sec  = period / 1000;
+    this->timerSpec.it_value.tv_nsec = (period % 1000) * 1000000;
+
+    if (mode == PERIODIC)
+    {
+        this->timerSpec.it_interval.tv_sec  = period / 1000;
+        this->timerSpec.it_interval.tv_nsec = (period % 1000) * 1000000;
+    }
+
+    this->sev.sigev_notify          = SIGEV_THREAD;
+    this->sev.sigev_notify_function = callbackWrapper;
+    this->sev.sigev_value.sival_ptr = this;
+
+    timer_create(CLOCKID, &this->sev, &this->timerId);
+}
+
+LinuxTimer::~LinuxTimer()
+{
+    free(name);
+    timer_delete(timerId);
+}
+
+bool LinuxTimer::start()
+{
+    return timer_settime(timerId, 0, &timerSpec, nullptr) == 0;
+}
+
+bool LinuxTimer::startFromISR()
+{
+    return start();
+}
+
+bool LinuxTimer::stop()
+{
+    itimerspec stopTimerSpec{};
+    return timer_settime(timerId, 0, &stopTimerSpec, nullptr) == 0;
+}
+
+bool LinuxTimer::stopFromISR()
+{
+    return stop();
+}
+
+[[nodiscard]] bool LinuxTimer::isRunning() const
+{
+    itimerspec currentSpec{};
+    timer_gettime(timerId, &currentSpec);
+    return currentSpec.it_value.tv_sec != 0 || currentSpec.it_value.tv_nsec != 0;
+}
+
+bool LinuxTimer::setPeriod(uint32_t newPeriod_ms)
+{
+    timerSpec.it_value.tv_sec  = newPeriod_ms / 1000;
+    timerSpec.it_value.tv_nsec = (newPeriod_ms % 1000) * 1000000;
+
+    if (timerSpec.it_interval.tv_sec != 0 || timerSpec.it_interval.tv_nsec != 0)
+    {
+        timerSpec.it_interval.tv_sec  = newPeriod_ms / 1000;
+        timerSpec.it_interval.tv_nsec = (newPeriod_ms % 1000) * 1000000;
+    }
+    return start();
+}
+
+bool LinuxTimer::setPeriodFromISR(uint32_t newPeriod_ms)
+{
+    return setPeriod(newPeriod_ms);
+}
+
+[[nodiscard]] uint32_t LinuxTimer::getPeriod() const
+{
+    return timerSpec.it_value.tv_sec * 1000 + timerSpec.it_value.tv_nsec / 1000000;
+}
+
+[[nodiscard]] LinuxTimer::Mode LinuxTimer::getMode() const
+{
+    return (timerSpec.it_interval.tv_sec == 0 && timerSpec.it_interval.tv_nsec == 0) ? ONE_SHOT : PERIODIC;
+}
+
+[[nodiscard]] uint32_t LinuxTimer::getTimeout() const
+{
+    itimerspec currentSpec{};
+    timer_gettime(timerId, &currentSpec);
+    return currentSpec.it_value.tv_sec * 1000 + currentSpec.it_value.tv_nsec / 1000000;
+}
+
+[[nodiscard]] uint32_t LinuxTimer::getTimeoutTime() const
+{
+    itimerspec currentSpec{};
+    timer_gettime(timerId, &currentSpec);
+    timespec now{};
+    clock_gettime(CLOCKID, &now);
+    return (currentSpec.it_value.tv_sec + now.tv_sec) * 1000 + (currentSpec.it_value.tv_nsec + now.tv_nsec) / 1000000;
+}
---
+++ Source/include/LinuxTimer.h
@@ -0,0 +1,57 @@
+#ifndef LINUXTIMER_H
+#define LINUXTIMER_H
+
+#include "OSInterface.h"
+#include "OSInterface_Timer.h"
+
+#include <signal.h>
+#include <time.h>
+
+#define CLOCKID CLOCK_REALTIME
+
+class LinuxTimer final : public OSInterface_Timer
+{
+public:
+    LinuxTimer(uint32_t period, OSInterface_Timer::Mode mode, OSInterfaceProcess callback, void* callbackArg,
+               const char* timerName);
+
+    ~LinuxTimer() override;
+
+    bool start() override;
+
+    bool startFromISR() override;
+
+    bool stop() override;
+
+    bool stopFromISR() override;
+
+    [[nodiscard]] bool isRunning() const override;
+
+    bool setPeriod(uint32_t newPeriod_ms) override;
+
+    bool setPeriodFromISR(uint32_t newPeriod_ms) override;
+
+    [[nodiscard]] uint32_t getPeriod() const override;
+
+    [[nodiscard]] Mode getMode() const override;
+
+    [[nodiscard]] uint32_t getTimeout() const override;
+
+    [[nodiscard]] uint32_t getTimeoutTime() const override;
+
+private:
+    char* name;
+
+    timer_t timerId{};
+
+    OSInterfaceProcess callbacFunction{};
+    void*              callbackArg{};
+
+    sigevent sev{};
+
+    itimerspec timerSpec{};
+
+    static void callbackWrapper(union sigval sv);
+};
+
+#endif // LINUXTIMER_H
