8901af7c83dd243e9d56d04b8aaa5f459b3bffa4
aom20021 <a.ollero@zaleos.net>
2026-03-07 13:44:07 +0100

    fix format

--- Source/include/LinuxUntypedQueue.h
+++ Source/include/LinuxUntypedQueue.h
@@ -3,8 +3,8 @@
 
 #include "OSInterface_UntypedQueue.h"
 
-#include <mqueue.h>
 #include <atomic>
+#include <mqueue.h>
 
 class LinuxUntypedQueue final : public OSInterface_UntypedQueue
 {
@@ -38,11 +38,11 @@ public:
     bool sendToFrontFromISR(const void* message) override;
 
 private:
-    mqd_t                  mqd{};
-    char                   queueName[256]{};
-    uint32_t               maxMessages;
-    uint32_t               messageSize;
-    std::atomic<uint32_t>  currentPriority;
+    mqd_t                 mqd{};
+    char                  queueName[256]{};
+    uint32_t              maxMessages;
+    uint32_t              messageSize;
+    std::atomic<uint32_t> currentPriority;
 
     bool            createQueue();
     void            deleteQueue();
