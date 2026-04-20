9f5c9586fe83ef23d1b194ea6ddef9887100ddaa
Alexander Zerpa <alexander.zerpa@purifymywater.org>
2026-04-20 19:23:57 +0200

    style(LinuxMutex): code format

--- Source/LinuxMutex.cpp
+++ Source/LinuxMutex.cpp
@@ -27,8 +27,8 @@ LinuxMutex::LinuxMutex(bool& result)
 
 LinuxMutex::~LinuxMutex()
 {
-    pthread_mutex_trylock(&mutex);  // blocking mutex if unlocked as unlocking an unlocked mutex is undefined behavior
-    signal();                       // make sure to unblock mutex before destroying it
+    pthread_mutex_trylock(&mutex); // blocking mutex if unlocked as unlocking an unlocked mutex is undefined behavior
+    signal();                      // make sure to unblock mutex before destroying it
     if (const error_t res = pthread_mutex_destroy(&mutex); res != 0)
     {
         OSInterfaceLogError("LinuxOSInterface", "Failed to destroy mutex: %s", strerror(res));
